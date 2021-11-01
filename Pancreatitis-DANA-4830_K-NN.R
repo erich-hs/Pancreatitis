4c2f8d296d1665fa227736a4aa75926dfbd72ce3
Felix <77309049+LFelixhz@users.noreply.github.com>
2021-11-01 11:47:32 -0700

    Subsetting according to treatment

--- Pancreatitis-DANA-4830_K-NN.R
+++ Pancreatitis-DANA-4830_K-NN.R
@@ -4,3 +4,13 @@ pacman::p_load(tidyverse, olsrr, forecast, corrr, caret, GGally,
 setwd("~/Langara/DANA 4830 - 001/Assignment 2/Pancreatitis")
 
 missForest_df <- read.csv('~/Langara/DANA 4830 - 001/Assignment 2/Pancreatitis/data/final_models/missForest_df.csv', stringsAsFactors = TRUE)
+
+summary(missForest_df)
+
+#Sub-setting the df by treatment
+pext <- subset(missForest_df,missForest_df$pex == 'PEX Treatment')
+stdt <- subset(missForest_df,missForest_df$pex == 'Standard Treatment')
+
+#Computing summary statistics for each treatment group
+summary(pext)
+summary(stdt)
